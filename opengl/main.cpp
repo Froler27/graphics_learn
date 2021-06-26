b5c1866b1bc9edca320c3600a78bfb67a97213ed
Froler27 <wqpyihu@163.com>
2021-06-26 16:54:52 +0800

    添加点光源

--- opengl/main.cpp
+++ opengl/main.cpp
@@ -33,6 +33,8 @@ bool firstMouse = true;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
 int main(int argc, char** argv)
 {
 	glfwInit();
@@ -67,7 +69,7 @@ int main(int argc, char** argv)
 	glEnable(GL_DEPTH_TEST);
 
 	Shader lightingShader((strShaderFilePre + "4.1.lighting_maps.vs.shader").c_str(),
-		(strShaderFilePre + "5.1.light_casters.fs.shader").c_str());
+		(strShaderFilePre + "5.2.light_casters.fs.shader").c_str());
 	Shader lightCubeShader((strShaderFilePre + "4.1.light_cube.vs.shader").c_str(),
 		(strShaderFilePre + "4.1.light_cube.fs.shader").c_str());
 	
@@ -177,13 +179,16 @@ int main(int argc, char** argv)
 
 		// be sure to activate shader when setting uniforms/drawing objects
 		lightingShader.use();
-		lightingShader.setVec3("light.direction", -0.2f, -1.0f, -0.3f);
+		lightingShader.setVec3("light.position", lightPos);
 		lightingShader.setVec3("viewPos", camera.Position);
 
 		// light properties
 		lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
 		lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
 		lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+		lightingShader.setFloat("light.constant", 1.0f);
+		lightingShader.setFloat("light.linear", 0.09f);
+		lightingShader.setFloat("light.quadratic", 0.032f);
 
 		// material properties
 		lightingShader.setFloat("material.shininess", 32.0f);
@@ -224,13 +229,13 @@ int main(int argc, char** argv)
 		}
 
 		// also draw the lamp object
-		//lightCubeShader.use();
-		//lightCubeShader.setMat4("projection", projection);
-		//lightCubeShader.setMat4("view", view);
-		//model = glm::mat4(1.0f);
-		//model = glm::translate(model, lightPos);
-		//model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
-		//lightCubeShader.setMat4("model", model);
+		lightCubeShader.use();
+		lightCubeShader.setMat4("projection", projection);
+		lightCubeShader.setMat4("view", view);
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, lightPos);
+		model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
+		lightCubeShader.setMat4("model", model);
 
 		glBindVertexArray(lightCubeVAO);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
